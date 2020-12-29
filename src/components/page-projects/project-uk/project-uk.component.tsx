7a8acac4cca5d43414b6ec3d5ad8da1cb2901c96
unknown <mdarif.afi@gmail.com>
2020-12-29 20:36:25 +0530

    project uk added

--- src/components/page-projects/project-uk/project-uk.component.tsx
+++ src/components/page-projects/project-uk/project-uk.component.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./project-uk.style.scss";
 
 export default class ProjectsUK extends React.Component {
     render(){
         return (
-           <>
+           <div className="country-uk">
             <div className="tantum-block project-beach">
                 <div className="address-project">
                     <div className="project-title">
@@ -27,6 +28,7 @@ export default class ProjectsUK extends React.Component {
                     </div>
                 </div>
                 <div className="project-description">
+                <span className="seen-beach"></span>
                     <p>An above market price sale was just what the doctor ordered for one disgruntled homeowner. The client was midway through an external extension project, but had been let down by contractors.</p>
                     <p>As is sadly commonplace with many extension projects, the client’s contractors deviated from their original plan. This resulted in going over budget, and a breach of planning regulations. The extension had come to a standstill.</p>
                     <p>We revised both the external extension and internal design to bring the project back in line with regulations. Plans were resubmitted to local authorities to rectify the previous contractors’ mistakes. The work went ahead as planned.</p>
@@ -34,7 +36,40 @@ export default class ProjectsUK extends React.Component {
                     <p>When the renovation was finished, the owner decided to put the property on the market. We’re delighted to say it sold for above market value. More importantly, we restored the owner’s faith in project management teams, delivering high quality from start to finish.</p>
                 </div>
             </div>
-           </>
+            <div className="tantum-block project-post-office">
+                <div className="address-project">
+                    <div className="project-title">
+                        <h5>Extension project rescue in Deal,Kent</h5>
+                    </div>
+                    <div className="project-detail">
+                        <div>
+                        <div>
+                            <div>Type:</div>
+                            <div>Penthouse Extension</div>
+                        </div>
+                        <div>
+                            <div>Duration:</div>
+                            <div>4 Months</div>
+                        </div>
+                        <div>
+                            <div>Cost:</div>
+                            <div>£125,000</div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="project-description">
+                    <span className="seen-statium"></span>
+                    <p>A little ingenuity and vision turned a modest two-storey building into a modern apartment block. Our client approached us to project manage a ground floor extension and first floor office conversion in an old Post Office.</p>
+                    <p>The building’s office space would be converted to a flat, while the ground floor would also become a larger living area. Using our market experience, we carried out a thorough survey and market study. This led us to propose a different approach. Instead, we suggested extending two more storeys up.</p>
+                    <p>Our new plans offered the client the chance to maximise the available space. Rather than offering just three apartments, the new conversion would open up an incredible five more flats. We carried out the necessary checks and offered up our most talented contractors.</p>
+                    <p>After completing the project, we took it one step further. We oversaw a basement conversion, resulting in four modern, attractive offices. Our client, a renowned property developer, was thrilled with the results. With new opportunities for both residential and office rental space, he made a significant return on investment. For us, that was the perfect testimonial.</p>
+                </div>
+            </div>
+            <div className="tantum-block more-info">
+                <a target="_blank" href="https://www.flickr.com/photos/147240716@N08/albums/with/72157679505805946">See more projects</a>
+            </div>
+           </div>
         )
     }
 }
