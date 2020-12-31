d5371d8d24d326d2be4986c1c74afddca1ee5031
unknown <mdarif.afi@gmail.com>
2020-12-31 16:02:11 +0530

    validation added

--- src/components/page-contact-us/contact-us.component.tsx
+++ src/components/page-contact-us/contact-us.component.tsx
@@ -1,13 +1,98 @@
 import React from "react";
 import "./contact-us.scss";
 
-export default class ContactUs extends React.Component {
-
-    email:string | undefined;
-
-    setEmail = (e:React.ChangeEvent) => {
-        e.preventDefault
+interface FormFields {
+    fields:{
+        name:string;
+        email:string;
+        phone:string|number;
+        option:string;
+        message:string;
+    },
+    errors:{
+        [key:string] : string
     }
+}
+
+export default class ContactUs extends React.Component<FormFields> {
+
+    state:FormFields;
+
+    constructor(props: FormFields){
+        super(props);
+
+        this.state = {
+            fields: {
+                name:"",
+                email:"",
+                phone:"",
+                option:"",
+                message:""
+            },
+            errors: {
+                name:"",
+                email:"",
+                phone:"",
+                option:"",
+                message:""
+            }
+        }
+    }
+
+    contactSubmit(e:React.FormEvent<HTMLFormElement>):void {
+        e.preventDefault();
+        if(this.fieldValidation()){
+            alert("Form submitted");
+          }else{
+            alert("Form has errors.")
+          }
+      
+        console.log("form values", this.state);
+    }
+
+    fieldValidation(){
+        let fields = this.state.fields;
+        let errors:any = {};
+        let formIsValid = true;
+
+        if(!fields["name"]){
+            formIsValid = false;
+            errors['name'] = "Cannot be empty";
+        }
+
+        if(fields["name"].trim().length){
+            if(!fields["name"].match(/^[a-zA-Z]+$/)){
+              formIsValid = false;
+              errors["name"] = "Only letters";
+            }      	
+        }
+
+        if(!fields["email"]){
+            formIsValid = false;
+            errors["email"] = "Cannot be empty";
+          }
+
+        if(typeof fields["email"] !== "undefined" && fields["email"].length){
+            let lastAtPos = this.state.fields["email"].lastIndexOf('@');
+            let lastDotPos = this.state.fields["email"].lastIndexOf('.');
+            if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
+                formIsValid = false;
+                errors["email"] = "Email is not valid";
+            }
+        }
+        console.log(this.state);
+        this.setState(() => ({...this.state.errors, errors:errors}));
+        return formIsValid;
+
+    }
+
+    handleChange = (e:React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLSelectElement>, id: string ) =>{
+        e.preventDefault();
+        const {value} = e.target;
+        this.setState(() => ({fields: {...this.state.fields, [id]:value}}));
+    }
+
+
     render(){
         return(
             <div className="page-contact">
@@ -23,21 +108,44 @@ export default class ContactUs extends React.Component {
                             please fill in the form below
                             and we will be happy to arrange
                             a meeting or site visit.</p>
-                            <form>
-                                <label htmlFor="name"><input id="name" name="name" placeholder="Name" type="text"/></label>
+                            <form name="contactform" 
+                            className="contactform" 
+                            onSubmit= {this.contactSubmit.bind(this)} >
+                                <label htmlFor="name">
+                                    <input id="name" name="name" 
+                                    onChange={(e) => this.handleChange(e, "name")}
+                                    value={this.state.fields["name"]}
+                                    autoComplete="off"
+                                    placeholder="Name" type="text"/>
+                                </label>
+                                <span className="error">{this.state.errors["name"]}</span>
                                 <div>
-                                <label htmlFor="email"><input value={this.email}  placeholder="Email" onChange={(e) => this.setEmail(e)} type="email" name="email" id="email"/></label>
-                                <label htmlFor="phone"> <input type="tel" name="phone" placeholder="Phone" id="phone"/> </label>
+                                <label htmlFor="email">
+                                    <input placeholder="Email" 
+                                    onChange={(e) => this.handleChange(e, "email")}
+                                    value={this.state.fields["email"]}
+                                    type="email" name="email" id="email"/>
+                                </label>
+                                <span className="error">{this.state.errors["email"]}</span>
+                                <label htmlFor="phone"> 
+                                <input type="tel" name="phone" 
+                                onChange={(e) => this.handleChange(e, "phone")}
+                                value={this.state.fields["phone"]}
+                                placeholder="Phone" id="phone"/> </label>
                                 </div>
                                 <label htmlFor="options" className="select" >
-                                    <select name="options" id="options">
-                                        <option value="" disabled selected>Choose an option</option>
+                                    <select name="options" id="options" onChange={(e) => this.handleChange(e, "option")}>
+                                        <option value="">Choose an option</option>
                                         <option value="Metres">Metres</option>
                                         <option value="Feet">Feet</option>
                                         <option value="Fathoms">Fathoms</option>
                                     </select>
                                 </label>
-                                <label className="message" htmlFor="message"><textarea placeholder="Message" name="message" id="message"></textarea></label>
+                                <label className="message" htmlFor="message">
+                                    <textarea placeholder="Message" 
+                                    value={this.state.fields["message"]}
+                                    onChange={(e) => this.handleChange(e, "message")}
+                                    name="message" id="message"></textarea></label>
                                 <button type="submit">Send</button>
                             </form>
                             
